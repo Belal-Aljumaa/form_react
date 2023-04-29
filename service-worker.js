4e909e68d21aa4f8999984c6b318f716fcb55340
Belal-Aljumaa <bilal.joma90@gmail.com>
2023-04-29 04:05:22 +0200

    Progressive Web App (PWA)

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'my-cache-v1';
+
+const urlsToCache = [
+  '/',
+  'dist/index.html/',
+  'dist/assets/index-caa7d8d7.css',
+  'dist/assets/index-25e21767.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then((response) => {
+        if(!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
+    })
+  );
+});
